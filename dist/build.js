d4b84a778b91daf5cb9df71db9f174f8f8d181d0
hazelGranger <wangzhen_blcu@163.com>
2017-12-30 11:38:41 +0800

    add favicon

--- dist/build.js
+++ dist/build.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "0852f3b5b2a501296783"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "362933f6692455aba383"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -716,7 +716,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "https://hazelgranger.github.io/css-secrets-demo/dist/";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
